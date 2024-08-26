d481f722b6ddfb8905a40f291ddfbfc0e89a7583
chtello <chtello@escuelasproa.edu.ar>
2024-08-26 10:54:25 -0300

    casi

---
+++ Examen/26/8/24/src/App.jsx
@@ -0,0 +1,15 @@
+
+import TecGrilla from './componentes/Teclas'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <TecGrilla />
+      </div>
+  )
+}
+
+export default App
---
+++ Examen/26/8/24/src/componentes/Teclas.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './Teclas.css'
+function TecGrilla() {
+    return(
+        <div>
+            <input type="text" />
+    <div  class="contTeclas ">
+        <div class="tecla"><button>1</button></div>
+        <div class="tecla"><button>2</button></div>
+        <div class="tecla"><button>3</button></div>
+        <div class="tecla"><button>4</button></div>
+        <div class="tecla"><button>5</button></div>
+        <div class="tecla"><button>6</button></div>
+        <div class="tecla"><button>7</button></div>
+        <div class="tecla"><button>8</button></div>
+        <div class="tecla"><button>9</button></div>
+        <div class="tecla"><button>+</button></div>
+        <div class="tecla"><button>-</button></div>
+        <div class="tecla"><button>limpiar</button></div>
+        <div class="tecla"><button>x</button></div>
+        <div class="tecla"><button>/</button></div>
+        <div class="tecla"><button>igual</button></div>
+</div>
+</div>
+)
+
+}
+
+export default TecGrilla;
